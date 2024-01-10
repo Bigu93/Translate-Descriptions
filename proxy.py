7ace5d745ebe12d0a858d4b53c80ef1b1ef90140
Marcin Szymanek <marcinszymanek93@gmail.com>
2024-01-10 13:00:35 +0100

    Removed unused libraries

--- proxy.py
+++ proxy.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response, Response
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from dotenv import load_dotenv
 from openai import OpenAI
