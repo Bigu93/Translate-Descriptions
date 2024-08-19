10edae24eb80cacc078b30bd998ab46f1945dd50
Marcin Szymanek <marcinszymanek93@gmail.com>
2024-08-19 15:36:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from flask_cors import CORS
 from config import ALLOWED_ORIGINS
 from utils import get_logger, internal_server_error, invalid_json_format
-from tokens import is_token_valid, generate_token, check_all_tokens, token_bp
+from tokens import is_token_valid, generate_token, token_bp
 from proxy import handle_proxy_request
 from product import (
     handle_product_data_request,
